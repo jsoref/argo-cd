07926ae7059e1bf79c69633346967eed4570903b
Josh Soref <jsoref@gmail.com>
2026-03-12 09:07:32 -0400

    spelling: retrieving
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- util/db/db.go
+++ util/db/db.go
@@ -147,7 +147,7 @@ func (db *db) GetApplicationControllerReplicas() int {
 	if err != nil {
 		appControllerDeployment = nil
 		if !apierrors.IsNotFound(err) {
-			log.Warnf("error retrieveing Argo CD controller deployment: %s", err)
+			log.Warnf("error retrieving Argo CD controller deployment: %s", err)
 		}
 	}
 	if appControllerDeployment != nil && appControllerDeployment.Spec.Replicas != nil {
