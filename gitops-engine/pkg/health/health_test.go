69f1253213eff73c09377236880bb5e40458628c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-08-14 16:28:17 -0400

    Treat WaitForFirstConsumer Pending as Healthy

--- gitops-engine/pkg/health/health_test.go
+++ gitops-engine/pkg/health/health_test.go
@@ -54,7 +54,7 @@ func TestDaemonSetOnDeleteHealth(t *testing.T) {
 
 func TestPVCHealth(t *testing.T) {
 	assertAppHealth(t, "./testdata/pvc-bound.yaml", HealthStatusHealthy)
-	assertAppHealth(t, "./testdata/pvc-pending.yaml", HealthStatusProgressing)
+	assertAppHealth(t, "./testdata/pvc-pending.yaml", HealthStatusHealthy)
 }
 
 func TestServiceHealth(t *testing.T) {
